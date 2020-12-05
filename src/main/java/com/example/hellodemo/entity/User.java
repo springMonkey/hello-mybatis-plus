b21ebf546fd87d09e527b5a47c2e01137bb63800
yangchn <yangchn@neusoft.com>
2020-12-05 14:55:25 +0800

    【feat】:项目初始化

---
+++ src/main/java/com/example/hellodemo/entity/User.java
@@ -0,0 +1,17 @@
+package com.example.hellodemo.entity;
+
+import lombok.Data;
+
+/**
+ * @author yangchn
+ * @Description
+ * @date 2020年12月05日
+ * Copyright neusoft (c) 2020 .
+ */
+@Data
+public class User {
+    private String id;
+    private String name;
+    private String age;
+    private String sex;
+}
---
+++ src/main/java/com/example/hellodemo/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.example.hellodemo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.hellodemo.entity.User;
+
+/**
+ * @author yangchn
+ * @Description
+ * @date 2020年12月05日
+ * Copyright neusoft (c) 2020 .
+ */
+public class UserMapper extends BaseMapper<User> {
+}
